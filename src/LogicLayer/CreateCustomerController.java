8c35c68764131b16bde5452d13750958cb5a648b
Thomas <holmegaard1996@gmail.com>
2017-05-11 14:40:29 +0200

    Minor changes. Still missing the add to table function

--- src/LogicLayer/CreateCustomerController.java
+++ src/LogicLayer/CreateCustomerController.java
@@ -3,6 +3,7 @@ package LogicLayer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 /**
@@ -14,25 +15,24 @@ public class CreateCustomerController {
     @FXML
     private TextField faknr1, dato1, kundenr1, debitor1, navn1, adresse1, beløb1;
 
-    private String fakture = faknr1.getText();
-//    private String dato = dato1.getText();
-//    private String kundenr = kundenr1.getText();
-//    private String debitor = debitor1.getText();
-//    private String navn = navn1.getText();
-//    private String adresse = adresse1.getText();
-//    private String beløb = beløb1.getText();
-//
+    ObservableList<CustomerInformation> kunder;
 
-//
-//    public ObservableList<CustomerInformation> saveData(){
-//        ObservableList<CustomerInformation> kunder = FXCollections.observableArrayList();
-//        //indsæt kundeinformationer her
-//        kunder.add(new CustomerInformation(fakture, dato, kundenr, debitor, navn, adresse, beløb));
-//        return kunder;
-//
-//    }
-    public void save(){
 
-        System.out.println("hhh");
+
+    public ObservableList<CustomerInformation> save(){
+
+        String fakture = faknr1.getText();
+        String dato = dato1.getText();
+        String kundenr = kundenr1.getText();
+        String debitor = debitor1.getText();
+        String navn = navn1.getText();
+        String adresse = adresse1.getText();
+        String beløb = beløb1.getText();
+
+
+        kunder = FXCollections.observableArrayList();
+        //indsæt kundeinformationer her
+        kunder.add(new CustomerInformation(fakture, dato, kundenr, debitor, navn, adresse, beløb));
+        return kunder;
     }
 }
--- src/LogicLayer/MainController.java
+++ src/LogicLayer/MainController.java
@@ -1,13 +1,24 @@
 package LogicLayer;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+
 /**
  * Created by Thomas on 07-05-2017.
  */
 public class MainController extends OpenNewWindow {
 
+    @FXML
+    TableView table;
+
+    CreateCustomerController createCustomerController = new CreateCustomerController();
+
 
     public void createData(){
         newWindow("CreateCustomers.fxml");
+
+        table.setItems(createCustomerController.kunder);
 
     }
 }
